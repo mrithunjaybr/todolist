741f6389fc4a0afbcfd318cc726d610086d2391c
mrithunjaybr <mrithunjaybr@gmail.com>
2022-04-03 21:06:40 -0500

    Web security config to get to homepage done.

--- src/main/java/com/example/todolist/repository/UserRepository.java
+++ src/main/java/com/example/todolist/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.todolist.repository;
 import com.example.todolist.exception.ETAuthException;
 import com.example.todolist.model.User;
 
+
 public interface UserRepository {
     Integer createUser(String firstName, String lastName, String email, String password) throws ETAuthException;
 
@@ -11,4 +12,6 @@ public interface UserRepository {
     User findById(Integer userId);
 
     User findByEmailAndPassword(String email, String password) throws ETAuthException;
+
+    User findByEmail(String email);
 }
---
+++ src/main/java/com/example/todolist/services/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.todolist.services;
+
+import com.example.todolist.model.CustomUserDetails;
+import com.example.todolist.model.User;
+import com.example.todolist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null){
+            throw new UsernameNotFoundException("Email Id not valid!");
+        }
+        return new CustomUserDetails(user);
+    }
+}
